0cf979be9a4a5a95dbb19fb45b00d7cd4bb4e524
yoannakr <jkrmilkova@gmail.com>
2020-01-09 22:56:50 +0200

    added password hashing

--- src/main/java/services/implemations/HashingServiceImpl.java
+++ src/main/java/services/implemations/HashingServiceImpl.java
@@ -1,11 +1,12 @@
 package services.implemations;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import services.HashingService;
 
 public class HashingServiceImpl implements HashingService {
 
     @Override
     public String hash(String value) {
-        return "@" + value + "!";
+        return DigestUtils.sha256Hex(value);
     }
 }
